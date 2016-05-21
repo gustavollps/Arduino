fcabf3512c5b8200c96a80d7277e71d3548f3c54
gustavollps <gustavollps@gmail.com>
2016-05-21 03:52:16 -0300

    PID double

--- libraries/PID/PID.cpp
+++ libraries/PID/PID.cpp
@@ -9,7 +9,7 @@ void PID::start(){
 	OldTime = micros();
 }
 
-void PID::setParameters(float _kp,float _ki, float _kd, float _pid_min, float _pid_max){
+void PID::setParameters(double _kp,double _ki, double _kd, double _pid_min, double _pid_max){
 	kp = _kp;
 	ki = _ki;
 	kd = _kd;	
@@ -17,17 +17,17 @@ void PID::setParameters(float _kp,float _ki, float _kd, float _pid_min, float _p
 	pid_max = _pid_max;
 }
 
-void PID::setParameters(float _kp, float _ki, float _kd){
+void PID::setParameters(double _kp, double _ki, double _kd){
 	kp = _kp;
 	ki = _ki;
 	kd = _kd;	
 }
 
-void PID::changeSetPoint(float _setPoint){
+void PID::changeSetPoint(double _setPoint){
 	setPoint = _setPoint;
 }
 
-void PID::limitI(float _i_min, float _i_max){
+void PID::limitI(double _i_min, double _i_max){
 	i_min = _i_min;
 	i_max = _i_max;
 }
@@ -36,11 +36,11 @@ void PID::resetI(){
 	I = 0.0;
 }
 
-float PID::Compute(float read){
+double PID::Compute(double read){
 
 	NewTime = micros();
 	dTime = ((double)(OldTime - NewTime))/1000000.0;
-	float error = setPoint - read;
+	double error = setPoint - read;
 	dError = oError-error;
 
 	P = error*kp;
--- libraries/PID/PID.h
+++ libraries/PID/PID.h
@@ -5,22 +5,22 @@ class PID
 public:
 	PID();
 	void start();
-	void setParameters(float _kp,float _ki, float _kd, float _pid_min, float _pid_max);
-	void setParameters(float _kp,float _ki, float _kd);
-	float Compute(float read);
+	void setParameters(double _kp,double _ki, double _kd, double _pid_min, double _pid_max);
+	void setParameters(double _kp,double _ki, double _kd);
+	double Compute(double read);
 	void resetI();
-	float getP();
-	float getI();
-	float getD();
-	void limitI(float _i_min, float _i_max);
-	void changeSetPoint(float _setPoint);
-	float P;
-	float I;
-	float D;
-	float kp;
-	float ki;
-	float kd;
-	float _PID;
+	double getP();
+	double getI();
+	double getD();
+	void limitI(double _i_min, double _i_max);
+	void changeSetPoint(double _setPoint);
+	double P;
+	double I;
+	double D;
+	double kp;
+	double ki;
+	double kd;
+	double _PID;
 
 private:
 	
@@ -29,9 +29,9 @@ private:
 	double dTime;
 	double dError;
 	double oError;
-	float setPoint;
-	float pid_max;
-	float pid_min;
-	float i_min = 0;
-	float i_max = 0;
+	double setPoint;
+	double pid_max;
+	double pid_min;
+	double i_min = 0;
+	double i_max = 0;
 };
